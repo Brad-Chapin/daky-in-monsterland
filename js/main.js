df856e9c291bf06ee2b0f55c7ea9e936d4148ad4
Brad Chapin <paledoor@yahoo.com>
2017-02-27 15:11:09 -0700

    attempting grid tilemap

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $( document ).ready(function() {
-var game = new Phaser.Game(650, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+  // 700x480 game board with 28x32 sprite = 25x15 grid
+var game = new Phaser.Game(700, 480, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
   game.load.image("dakysprite", "assets/dakysprite.png");
+  game.load.tilemap("dakygrid", "assets//tiles/dakygrid.json", null, Phaser.Tilemap.TILED_JSON);
 }
 
 function create() {
-  game.add.sprite(0, 0, 'dakysprite');
+  game.add.tilemap("dakygrid");
+  game.add.sprite(650, 450, 'dakysprite');
 }
 
 function update() {
