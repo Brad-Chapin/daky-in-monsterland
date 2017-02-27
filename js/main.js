8695c2354aa2ace0ebc762e3a9eab22d820b53d0
Brad Chapin <paledoor@yahoo.com>
2017-02-27 11:29:04 -0700

    correcting image file names

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ $( document ).ready(function() {
 var game = new Phaser.Game(650, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image("dakySprite", "assets/dakySprite.png");
+  game.load.image("dakysprite", "assets/dakysprite.png");
 }
 
 function create() {
-  game.add.sprite(0, 0, 'dakySprite');
+  game.add.sprite(0, 0, 'dakysprite');
 }
 
 function update() {
