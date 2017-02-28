3648a68cc5587fb71c7647751f905f33f93ce53c
Brad Chapin <paledoor@yahoo.com>
2017-02-28 10:53:01 -0700

    minor styling tweaks, basic map render working

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 $( document ).ready(function() {
-  // 700x480 game board with 28x32 sprite = 25x15 grid
-var game = new Phaser.Game(704, 512, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+var game = new Phaser.Game(704, 512, Phaser.AUTO, 'daky', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.tilemap("dakymap", "assets/tiles/dakygrid.json", null, Phaser.Tilemap.TILED_JSON);
+  game.load.image("tiles", "assets/tiles/grass-tiles-2-small.png");
   game.load.image("dakysprite", "assets/dakysprite.png");
+
 }
-var grid;
+var map, layer;
 function create() {
+  map = game.add.tilemap("dakymap");
+  map.addTilesetImage("grass", "tiles");
+  layer = map.createLayer("layer1");
+  layer.resizeWorld();
+  game.add.sprite(100, 100, "dakysprite");
+
 }
 
 function update() {
 }
 });
+
+// BOARD_COLS = Math.floor(game.world.width / GEM_SIZE_SPACED);
+// BOARD_ROWS = Math.floor(game.world.height / GEM_SIZE_SPACED);
