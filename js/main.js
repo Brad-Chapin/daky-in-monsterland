d06622483b457df70a0080529b0875679c23c600
Brad Chapin <paledoor@yahoo.com>
2017-03-02 11:11:08 -0700

    monsters spawning, checking for collisions and correcting positions

--- js/main.js
+++ js/main.js
@@ -6,11 +6,16 @@ function preload() {
   game.load.tilemap("dakymap", "assets/tiles/DIMTileMap.json", null, Phaser.Tilemap.TILED_JSON);
   game.load.image("tiles", "assets/tiles/terrain.png");
   game.load.image("dakysprite", "assets/spritedakyfront.png");
+  game.load.image("monst1", "assets/monst1.png");
+  game.load.image("monst2", "assets/monst2.png");
+  game.load.image("monst3", "assets/monst3.png");
 
 }
-var map, layer, player;
+var map, layer, player, monsters;
+var moved = false;
 
 function create() {
+
   map = game.add.tilemap("dakymap");
   map.addTilesetImage("Ground", "tiles");
   layer = map.createLayer("Cliffside");
@@ -20,6 +25,51 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.arcade.enable(player);
   player.body.collideWorldBounds = true;
+
+  monsters = game.add.group();
+  monsters.enableBody = true;
+
+  for (let i = 0; i < 6; i++){
+    let monster;
+    let whichMonst = Math.floor(Math.random()*3);
+    let x = Math.floor(Math.random()*21)*32;
+    let y = Math.floor(Math.random()*15)*32;
+    if ((x == 352) && (y == 224)){
+      x+= 64;
+      y+= 64;
+    }
+    if (whichMonst == 0){
+      monster = monsters.create (x, y, "monst1");
+      game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    }
+    else if (whichMonst == 1){
+      monster = monsters.create (x, y, "monst2");
+      game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    }
+    else {
+      monster = monsters.create (x, y, "monst3");
+      game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    }
+    // switch (whichMonst) {
+    //   case 0:
+    //     monster = monsters.create (x, y, "monst1");
+    //     (game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    //     break;
+    //   case 1:
+    //     monster = monsters.create (x, y, "monst2");
+    //     (game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    //     break;
+    //   case 2:
+    //     monster = monsters.create (x, y, "monst3");
+    //     (game.physics.arcade.overlap(monster, monsters, moveIt, null, this);
+    //     break;
+    // }
+  }
+
+  function moveIt (){
+    x+= 64;
+    y+= 64;
+  }
 
   // controls start
   $("#fire").on("click", function (){
@@ -88,5 +138,6 @@ function update() {
 }
 });
 
-// BOARD_COLS = Math.floor(game.world.width / GEM_SIZE_SPACED);
-// BOARD_ROWS = Math.floor(game.world.height / GEM_SIZE_SPACED);
+// Math.floor(Math.random()*21)*32 returns //(0-22)*32
+// Math.floor(Math.random()*15)*32 returns //(0-15)*15
+// Math.floor(Math.random()*3) //returns 0-2
